091d1f2af87aaca592f4a12ab3ca94272041743f
niesssiobhan <niesssiobhan@live.com>
2018-12-26 13:20:35 -0800

    small changes

--- code-challenges/arrays/arrayShift/array-shift.js
+++ code-challenges/arrays/arrayShift/array-shift.js
@@ -6,14 +6,14 @@ secondCodeChallenge.insertShiftArray = function(arr, val) {
   let result = [];
   let mid = arr.length / 2;
 
-  for(let i = 0; i <= arr.length; i++){
-    if(i === mid){
+  for(let i = 0; i <= arr.length; i++) {
+    if(i === mid) {
       result[i] = val;
     }
-    if(i < mid){
+    if(i < mid) {
       result[i] = arr[i];
     }
-    if(i > mid){
+    if(i > mid) {
       result[i] = arr[i-1];
     }
   }
