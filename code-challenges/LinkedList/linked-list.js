07b3ac5d757c14f9e30fc6059d95863e7e44ac7d
niesssiobhan <niesssiobhan@live.com>
2018-12-28 18:43:32 -0800

    added in more of the methods

--- code-challenges/LinkedList/linked-list.js
+++ code-challenges/LinkedList/linked-list.js
@@ -30,11 +30,56 @@ class LinkedList {
     current.next = node;
   }
 
+// This will let you to be able to locate a value within the Linked List from the end and find its index value
+  fromEnd(k) {
+    if(!this.head) {
+      throw "Error";
+    }
+    let current = this.head;
+    let counter = 0;
+    current = this.head;
+    counter = 0;
+    while(counter !== k) {
+      current = current.next;
+      counter++;
+    }
+    return current.value;
+  }
+
 // This will add a new Node with the value to the head
   insert(value) {
     let node = new Node(value);
     node.next = this.head;
     this.head = node;
+  }
+
+// This is going to make it so that you are able to insert a value before another
+  insertBefore(value, newValue) {
+    let node = new Node(newValue);
+    let current = this.head;
+    if(!this.head) {
+      return;
+    }
+    while(current.next !== value) {
+      current = current.next;
+    }
+    node.next = current.next;
+    current.next = node;
+  }
+
+// This will make it so that you are able to insert a value after another
+  insertAfter(value, newValue){
+    if(!this.head){
+      this.head = new Node(newValue);
+      return;
+    }
+    let currert = this.head;
+    while(current.value !== value){
+      current = current.next;
+    }
+    let node = new Node(newValue);
+    node.next = current.next;
+    current.next = node;
   }
 
 // This will check if there is value that exists in the list
