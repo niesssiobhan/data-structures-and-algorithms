c26ecc5e446a4e6ed2794ba74119f51ecaf65f0b
niesssiobhan <niesssiobhan@live.com>
2019-01-16 14:28:09 -0800

    code challenge

---
+++ code-challenges/tree/__tests__/findMaxValue.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const tree =  require('../tree/tree.js');
+
+describe('tree', () => {
+  describe('findMaxValue', () => {
+    it('will return the largeest value that is found in the tree', () => {
+      let 
+    })
+  })
+})
